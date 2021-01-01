7cf7168028a9197a439362ac10ae1054fee9e4cd
unknown <nigarofe@gmail.com>
2021-01-01 09:17:17 -0300

    Framerate Problem

--- JogoDoPo/Population.js
+++ JogoDoPo/Population.js
@@ -12,17 +12,40 @@ class Population {
         }
     }
 
-    addEntity(x, y) {
-        // Just add a paticle if there is not already a particle in that place
-        let alreadyOccupied = false;
-        for (let i = 0; i < this.entities.length; i++) {
-            if (mouseX == this.entities[i].position.x && mouseY == this.entities[i].position.y) {
-                alreadyOccupied = true;
-                break;
+    addEntity(x, y, r) {
+        for (let h = x - r; h <= x + r; h++) {
+            for (let v = y - r; v <= y + r; v++) {
+                let xNow = h;
+                let yNow = v;
+
+                // Just add a paticle if there is not already a particle in that place
+                let alreadyOccupied = false;
+                for (let i = 0; i < this.entities.length; i++) {
+                    if (xNow == this.entities[i].position.x && yNow == this.entities[i].position.y) {
+                        alreadyOccupied = true;
+                        break;
+                    }
+                }
+                if (!alreadyOccupied) {
+                    this.entities.push(new Particle(xNow, yNow));
+                }
             }
         }
-        if (!alreadyOccupied) {
-            this.entities.push(new Particle(mouseX, mouseY));
-        }
+
+        console.log(this.entities.length)
     }
+
+    // addEntity(x, y) {
+    //     // Just add a paticle if there is not already a particle in that place
+    //     let alreadyOccupied = false;
+    //     for (let i = 0; i < this.entities.length; i++) {
+    //         if (x == this.entities[i].position.x && y == this.entities[i].position.y) {
+    //             alreadyOccupied = true;
+    //             break;
+    //         }
+    //     }
+    //     if (!alreadyOccupied) {
+    //         this.entities.push(new Particle(mouseX, mouseY));
+    //     }
+    // }
 }
--- JogoDoPo/sketch.js
+++ JogoDoPo/sketch.js
@@ -1,15 +1,22 @@
 let p;
+let brushSize = 2;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    frameRate(30);
     p = new Population();
 }
 
 function draw() {
     background(0);
+
+    fill(255);
+    textSize(30);
+    text(Math.round(frameRate()), 20, 50);
+
     p.render();
 }
 
 function mouseDragged() {
-    p.addEntity(mouseX, mouseY);
+    p.addEntity(mouseX, mouseY, brushSize);
 }
